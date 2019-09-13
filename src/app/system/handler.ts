6f1b511d1b6847756b560ee1ea41b70d3f58dcaf
ramses132 <yugo132@gmail.com>
2019-09-13 13:36:56 -0500

    [ADD] - system

---
+++ src/app/system/handler.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import * as bodyParser from 'body-parser';
+import * as uuidV4 from 'uuid/v4';
+import * as configModule from '../common/config-manager/config';
+
+export const hello: APIGatewayProxyHandler = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+}
